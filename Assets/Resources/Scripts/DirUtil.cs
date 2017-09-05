04f83104f42211089f774f186225af4a1f88e8f9
syun77 <2dgames.jp@gmail.com>
2017-09-06 00:22:50 +0900

    プレイヤーのカーソル移動を実装

---
+++ Assets/Resources/Scripts/DirUtil.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 移動方向定数
+/// </summary>
+public enum eDir {
+	None, // 無効な方向
+
+	Left, // 左
+	Up, // 上
+	Right, // 右
+	Down, // 下
+};
+
+/// <summary>
+/// 移動方向のユーティリティ
+/// </summary>
+public class DirUtil {
+	public static Vector2 ToVecWorld(eDir Dir) {
+		Vector2 v = new Vector2(0, 0);
+		switch (Dir) {
+		case eDir.Left:
+			v.x = -1;
+			break;
+		case eDir.Up:
+			v.y = 1;
+			break;
+		case eDir.Right:
+			v.x = 1;
+			break;
+		case eDir.Down:
+			v.y = -1;
+			break;
+		}
+
+		return v;
+	}
+}
--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -7,12 +7,15 @@ using UnityEngine;
 /// </summary>
 public class Player : MonoBehaviour {
 
-[SerializeField]
-	float MOVE_SPEED = 0.1f;
+	[SerializeField]
+	float _MOVE_SPEED = 0.1f; // 移動速度
+
+	[SerializeField]
+	eDir _Dir; // 移動方向
 
 	// Use this for initialization
 	void Start () {
-		
+		_Dir = eDir.Down;
 	}
 	
 	/// <summary>
@@ -20,39 +23,74 @@ public class Player : MonoBehaviour {
 	/// </summary>
 	void Update () {
 		// 移動
-		UpdateMove ();
+		_UpdateMove ();
+		// アニメーションの更新
+		_UpdateAnimation();
 	}
 
 	/// <summary>
 	/// 更新・移動
 	/// </summary>
-	void UpdateMove() {
+	void _UpdateMove() {
 		Vector3 p = transform.position;
+		eDir Dir = eDir.None;
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			// 上キーを押している
-			p.y += MOVE_SPEED;
+			Dir = eDir.Up;
 		} else if (Input.GetKey (KeyCode.LeftArrow)) {
-			// 左キーをしている
-			p.x -= MOVE_SPEED;
+			// 左キーを押している
+			Dir = eDir.Left;
 		} else if (Input.GetKey (KeyCode.DownArrow)) {
-			// 下キーをしている
-			p.y -= MOVE_SPEED;
+			// 下キーを押している
+			Dir = eDir.Down;
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
-			// 右キーをしている
-			p.x += MOVE_SPEED;
+			// 右キーを押している
+			Dir = eDir.Right;
 		}
+		Vector2 v = DirUtil.ToVecWorld (Dir);
+		if (Dir != eDir.None) {
+			_Dir = Dir;
+		}
+		p.x += v.x * _MOVE_SPEED;
+		p.y += v.y * _MOVE_SPEED;
 
 		// 移動量反映
 		transform.position = p;
+	}
 
-		ChangeSprite (1);
+	/// <summary>
+	/// アニメーションの番号を取得する
+	/// </summary>
+	/// <returns>アニメーション番号</returns>
+	int _GetAnimationIdx() {
+		switch (_Dir) {
+		case eDir.Left:
+			return 0;
+		case eDir.Up:
+			return 1;
+		case eDir.Right:
+			return 2;
+		case eDir.Down:
+			return 3;
+		default:
+			return 3;
+		}
+	}
+
+	/// <summary>
+	/// アニメーションの更新
+	/// </summary>
+	void _UpdateAnimation() {
+		int idx = _GetAnimationIdx ();
+		idx *= 4;
+		_ChangeSprite (idx);
 	}
 
 	/// <summary>
 	/// スプライトの番号を変更する
 	/// </summary>
 	/// <param name="idx">スプライト番号</param>
-	void ChangeSprite(int idx) {
+	void _ChangeSprite(int idx) {
 
 		// スプライトを取得する
 		Sprite[] sprites = Resources.LoadAll<Sprite> ("Sprites/chara");
