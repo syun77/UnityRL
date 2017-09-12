348580354ea19f246ef63ba866fce95d9a6c2b50
syun77 <2dgames.jp@gmail.com>
2017-09-12 22:43:19 +0900

    メッセージをキューで表示する fixed #8

--- Assets/Scripts/UI/MessageText.cs
+++ Assets/Scripts/UI/MessageText.cs
@@ -5,23 +5,45 @@ using UnityEngine.UI;
 
 public class MessageText : MonoBehaviour {
 
+  // テキストキューの最大数
+  public int MAX_TEXT_QUEUE;
+  // テキスト表示時間
+  public int TIMER_TEXT;
+
   Queue<string> _Pool = null;
+  int _TimerText = 0;
 
   /// <summary>
   /// テキスト追加
   /// </summary>
   /// <param name="str">String.</param>
   public void AddText(string str) {
+    if (_Pool.Count >= MAX_TEXT_QUEUE) {
+      // キューの最大を超えていたら取り除く
+      _Pool.Dequeue();
+    }
+
     _Pool.Enqueue (str);
+    _TimerText = TIMER_TEXT;
   }
 
 	// Use this for initialization
 	void Start () {
-    _Pool = new Queue<string> (4);
+        _Pool = new Queue<string> (MAX_TEXT_QUEUE);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+    if (_Pool.Count > 0) {
+      _TimerText--;
+      if (_TimerText == 0) {
+        // キューから取り出し
+        _Pool.Dequeue();
+        _TimerText = TIMER_TEXT / 4;
+      }
+    }
+
     var text = GetComponent<Text> ();
     var str = "";
     foreach (string s in _Pool) {
