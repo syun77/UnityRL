4ee9cc6d548a9a3a92e9159a8e9a221d39676441
syun77 <2dgames.jp@gmail.com>
2017-09-14 00:03:32 +0900

    GameManagerオブジェクトの作成 fixed #12

---
+++ Assets/Scripts/SequenceManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲームシーケンス管理
+/// </summary>
+public class SequenceManager : MonoBehaviour {
+
+  public Player player = null;
+
+	// Use this for initialization
+	void Start () {
+    player = player.GetComponent<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
